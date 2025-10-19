f4bf42d0560d0226815629433fc89f68efaf81a3
minjae1025 <kjt081025@gmail.com>
2025-10-20 01:58:45 +0900

    feat: 회원가입, 로그인 구현

--- CodeIN/urls.py
+++ CodeIN/urls.py
@@ -16,8 +16,13 @@ Including another URLconfS
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+# from user.views import user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-]
+    path('user/', include('user.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
