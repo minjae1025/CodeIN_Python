6701064099b57597b02a83f85ae102848f158740
minjae1025 <kjt081025@gmail.com>
2025-10-22 08:22:34 +0900

    feat: bootstrap 다운로드 및 login 페이지 제작

--- CodeIN/user/urls.py
+++ CodeIN/user/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
+app_name = 'user'
+
 urlpatterns = [
     path('sign_up/', views.sign_up, name='sign_up'),
     path('logout/', views.logout_view, name='logout'),
-    path('login/', auth_views.LoginView.as_view(template_name='user/login_form.html'), name='log_in'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login_form.html'), name='login'),
     path('mypage/', views.mypage_view, name='mypage'),
 ]
