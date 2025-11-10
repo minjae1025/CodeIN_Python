07b30a45a7f0f9c2c119e11a74309f95b9366da6
minjae1025 <kjt081025@gmail.com>
2025-11-10 23:41:13 +0900

    feat: 문제 리스트 및 추가 페이지

--- CodeIN/main/models.py
+++ CodeIN/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Problem(models.Model):
+    title = models.CharField(max_length=100, help_text="문제 제목")
+    description = models.TextField(help_text="문제 설명")
+    created = models.DateTimeField(auto_now_add=True)
+    ip = models.GenericIPAddressField()
+    example = models.JSONField(
+        default=list,
+        help_text="문제 예제(테스트 케이스) 목록"
+    )
+    conditions = models.JSONField(
+        default=list,
+        help_text="문제 조건 목록"
+    )
+    answer_count = models.IntegerField(
+        default=0,
+        help_text="맞춘 사람"
+    )
+    difficulty = models.IntegerField(
+        help_text="문제 난이도"
+    )
+
+    def __str__(self):
+        return self.title
--- CodeIN/main/urls.py
+++ CodeIN/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('problems/', views.problems, name='problems'),
+    path('create_problem/', views.create_problem, name='create_problem'),
 ]
