82de9b078f20c829c06729b0ca3ca841044a88b5
jaehokang1007 <jaehokang1007@icloud.com>
2025-11-11 10:18:54 +0900

    issue 고침

--- CodeIN/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('problems/', views.problems, name='problems'),
-    path('create_problem/', views.create_problem, name='create_problem'),
-]
--- CodeIN/user/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import User  # 커스텀 유저 모델 임포트
-
-# 커스텀 유저 모델을 admin 사이트에 등록
-admin.site.register(User)
